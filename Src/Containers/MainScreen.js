7f0c2beefc3f41635bc609f48a52d086e84b7e33
Tommy Leong <tommymoleong@gmail.com>
2020-12-15 18:25:52 +0800

    Created UI for search screen

--- Src/Containers/MainScreen.js
+++ Src/Containers/MainScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native'
+import {Text, View, TextInput, FlatList, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import styles from './Styles/MainScreenStyle'
 
 const MainScreen = () => {
     return(
-        <View style={styles.container}>
-            <Text>
-                MainScreen
-            </Text>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>        
+                    <Text style={styles.searchTitle}>Search an employee</Text>
+                    <View style={styles.searchBox}>
+                        <TextInput
+                            style={{backgroundColor:'white', width: '50%'}}
+                            placeholder="Employee name"
+                        />
+                    </View>
+            </View>
+        </TouchableWithoutFeedback>
     )
 
 }
--- Src/Containers/Styles/MainScreenStyle.js
+++ Src/Containers/Styles/MainScreenStyle.js
@@ -2,8 +2,26 @@ import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:'pink'
+        flex: 1,
+        backgroundColor:'lightgray',
+        display: 'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+
+    searchTitle:{
+        fontSize: 18
+    }, 
+
+    searchBox:{
+        width: '100%',
+        display:'flex',
+        alignItems:'center',
+        paddingTop: '5%',
+        paddingBottom: '5%'
+
     }
+
 })
 
 export default styles;
