058f5d59fb9202c547a7f969cbe5218cbea85b07
Tommy Leong <tommymoleong@gmail.com>
2020-12-15 19:55:27 +0800

    - Update text field border color if input is empty or contains only space - navigate to Result screen if employeeName meets the criteria

--- Src/Containers/MainScreen.js
+++ Src/Containers/MainScreen.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import {Text, View, TextInput, FlatList, TouchableWithoutFeedback, Keyboard} from 'react-native'
+import React, {useState, useRef} from 'react';
+import {
+    Text,
+    View,
+    TextInput,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from 'react-native'
 import styles from './Styles/MainScreenStyle'
 
-const MainScreen = () => {
+
+const MainScreen = ({navigation}) => {
+
+    const [employeeName, setEmployeeName] = useState('')
+    const [missingName, setMissingName] = useState(false)
+    const searchEmpRef = useRef(null);
+
     return(
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <View style={styles.container}>        
                     <Text style={styles.searchTitle}>Search an employee</Text>
                     <View style={styles.searchBox}>
                         <TextInput
-                            style={{backgroundColor:'white', width: '50%'}}
+                            style={{
+                                backgroundColor:'white',
+                                width: '50%', 
+                                borderColor: missingName ? 'red' : 'transparent',
+                                borderWidth: 1
+                            }}
                             placeholder="Employee name"
+                            onChange={({nativeEvent})=>{
+                                setEmployeeName(nativeEvent.text);
+                            }}
+                            onSubmitEditing={async() => {
+                                if(employeeName.trim().length < 1) return setMissingName(true)
+                                setMissingName(false)
+                                navigation.navigate('Result', {searchEmployee: employeeName});
+                            }}
                         />
                     </View>
             </View>
